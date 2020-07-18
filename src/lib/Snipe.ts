75f813c4fb87ef78374b484ea96af4e449ee2f43
Sting Alleman <stingalleman@icloud.com>
2020-07-18 21:25:35 +0200

    stuff

--- src/lib/Snipe.ts
+++ src/lib/Snipe.ts
@@ -4,15 +4,10 @@ export class Snipe {
   hardware: HardwareManager;
 
   constructor(private snipeURL: string, private accessToken: string) {
-
   	this.hardware = new HardwareManager(snipeURL, accessToken);
   }
 
-  public getLoginData() {
-  	return `URL: ${this.snipeURL}\nToken: ${this.accessToken}`;
-  }
-
-  public static init(snipeURL: string, accessToken: string) {
-  	return new Snipe(snipeURL, accessToken);
-  }
+	// public static init(snipeURL: string, accessToken: string) {
+	// 	return new Snipe(snipeURL, accessToken);
+	// }
 }
