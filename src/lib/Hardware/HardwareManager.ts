c8b15c3866f1898a15013904edae3a024e79b45c
Sting Alleman <stingalleman@icloud.com>
2020-07-19 14:06:52 +0200

    URL params working

--- src/lib/Hardware/HardwareManager.ts
+++ src/lib/Hardware/HardwareManager.ts
@@ -9,49 +9,49 @@ import { Hardware } from "./Hardware";
 //		if (await result.status == "error") throw(`Error on checkin:\n${JSON.stringify(result, null, " ")}`);
 export class HardwareManager extends Manager {
 
- /**
+	/**
  	* Return list of assets
   * @param options options - Options to pass to the API
  */
-  async get(options?: HardwareOptions) {
-    options = Object.assign({ limit: 50 }, options || {});
-    const res = await fetch(getApiURL(this.snipeURL, `/hardware?limit=${options.limit}`), {
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${this.apiToken}`,
-        "Accept": "application/json",
-        "Content-Type": "Content-Type"
-      }
-    });
-    const result = await res.json();
-    if (await result.status == "error") throw (JSON.stringify(result, null, " "));
-    if (res.status !== 200) throw (JSON.stringify(result, null, " "));
+	async get(options?: HardwareOptions) {
+		options = Object.assign({ limit: 50 }, options || {});
+		const res = await fetch(getApiURL(this.snipeURL, "/hardware", options), {
+			method: "GET",
+			headers: {
+				"Authorization": `Bearer ${this.apiToken}`,
+				"Accept": "application/json",
+				"Content-Type": "Content-Type"
+			}
+		});
+		const result = await res.json();
+		if (await result.status == "error") throw (JSON.stringify(result, null, " "));
+		if (res.status !== 200) throw (JSON.stringify(result, null, " "));
 
-    const json: Response<IHardware> = result;
-    return json.rows.map(hardware => new Hardware(hardware));
-  }
+		const json: Response<IHardware> = result;
+		return json.rows.map(hardware => new Hardware(hardware));
+	}
 
 
 	/**
 	 * Get specific asset
 	 * @param id Asset ID
 	 */
-  async getID(id: number) {
-    const res = await fetch(getApiURL(this.snipeURL, `/hardware/${id}`), {
-      method: "GET",
-      headers: {
-        "Authorization": `Bearer ${this.apiToken}`,
-        "Accept": "application/json",
-        "Content-Type": "Content-Type"
-      }
-    });
-    const result = await res.json();
-    if (result.status == "error") throw (JSON.stringify(result, null, " "));
-    if (res.status !== 200) throw (JSON.stringify(result, null, " "));
+	async getID(id: number) {
+		const res = await fetch(getApiURL(this.snipeURL, `/hardware/${id}`), {
+			method: "GET",
+			headers: {
+				"Authorization": `Bearer ${this.apiToken}`,
+				"Accept": "application/json",
+				"Content-Type": "Content-Type"
+			}
+		});
+		const result = await res.json();
+		if (result.status == "error") throw (JSON.stringify(result, null, " "));
+		if (res.status !== 200) throw (JSON.stringify(result, null, " "));
 
-    const json: IHardware = result;
-    return new Hardware(json);
-  }
+		const json: IHardware = result;
+		return new Hardware(json);
+	}
 
 
 	/**
@@ -60,26 +60,26 @@ export class HardwareManager extends Manager {
 	 * @param note Note
 	 * @param location Location ID
 	 */
-  async checkin(id: number, note?: string, location_id?: number) {
-    const data = {
-      note: note,
-      location_id: location_id
-    };
-    const JSONdata = JSON.stringify(data);
-    const res = await fetch(getApiURL(this.snipeURL, `/hardware/${id}/checkin`), {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${this.apiToken}`,
-        "Accept": "application/json",
-        "Content-Type": "Content-Type"
-      },
-      body: JSONdata
-    });
-    const result = await res.json();
-    if (await result.status == "error") {
-      if (await result.messages == "That asset is already checked in.") throw ("Asset already checked in");
-      throw (`Error on checkin:\n${JSON.stringify(result, null, " ")}`);
-    }
-    return result;
-  }
+	async checkin(id: number, note?: string, location_id?: number) {
+		const data = {
+			note: note,
+			location_id: location_id
+		};
+		const JSONdata = JSON.stringify(data);
+		const res = await fetch(getApiURL(this.snipeURL, `/hardware/${id}/checkin`), {
+			method: "POST",
+			headers: {
+				"Authorization": `Bearer ${this.apiToken}`,
+				"Accept": "application/json",
+				"Content-Type": "Content-Type"
+			},
+			body: JSONdata
+		});
+		const result = await res.json();
+		if (await result.status == "error") {
+			if (await result.messages == "That asset is already checked in.") throw ("Asset already checked in");
+			throw (`Error on checkin:\n${JSON.stringify(result, null, " ")}`);
+		}
+		return result;
+	}
 }
