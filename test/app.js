c7242dccdf1d84fbe39872f1639d86f533b6d5d2
Sting Alleman <stingalleman@icloud.com>
2020-07-19 14:12:37 +0200

    app.js

--- test/app.js
+++ test/app.js
@@ -4,11 +4,10 @@ const config = require("./config.json");
 const snipe = new Snipe("https://aucom.alfrinklive.nl", config.token);
 
 async function func() {
-	// const data = await snipe.hardware.get({
-	// 	limit: 50
-	// });
-	// console.log(data);
-	console.log(await snipe.hardware.getID(5));
+	const data = await snipe.hardware.get({
+		limit: 5
+	});
+	console.log(data.length);
 }
 
 func();
